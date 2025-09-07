d6cd83445d4b64c225c70f4296c12b52d7688e3c
HuJhihSyun <cj654vmp@gmail.com>
2025-09-07 11:48:53 +0800

    [fix] ogImage 路徑調整

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,7 +6,7 @@
     ogTitle: '甜蜜出走日：愛語十三期家庭局一日小旅行',
     ogDescription:
       '親愛的十三期家庭局，在日常的忙碌與服事中，是時候牽起彼此的手，來一場輕鬆又充電的小旅行啦～一起拍照、放鬆、留下屬於你們的甜蜜盛夏回憶！',
-    ogImage: 'images/cover.jpg'
+    ogImage: '/images/cover.jpg'
   })
 
   const isLoading = ref<boolean>(true)
